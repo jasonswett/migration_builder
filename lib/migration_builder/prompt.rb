d8c6fb73952863e30b7089de6c7a8617973fe594
Jason Swett <jason@benfranklinlabs.com>
2019-12-05 09:54:23 -0500

    Change to enum_select.

--- lib/migration_builder/prompt.rb
+++ lib/migration_builder/prompt.rb
@@ -3,7 +3,7 @@ require 'tty-prompt'
 module MigrationBuilder
   class Prompt < TTY::Prompt
     def default_select(label, options)
-      select(label, options, per_page: 30)
+      enum_select(label, options, per_page: 30)
     end
   end
 end
